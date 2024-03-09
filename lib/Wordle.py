b3524da29cf5d823198467ecd7e2602b615e26c0
James Tingen <jotingen@gmail.com>
2024-03-09 16:43:54 -0500

    Make a solver

--- lib/Wordle.py
+++ lib/Wordle.py
@@ -1,7 +1,85 @@
 import random
 
+class Words:
+    words: list[str] = []
 
-class Wordle:
+    def __init__(
+        self,
+        word_list_file: str = "words.times.txt",
+    ):
+
+        with open(word_list_file) as f:
+            self.words = f.read().splitlines()
+
+
+class Guesses:
+    guesses: list[str] = []
+
+    def __init__(
+        self,
+        guess_list_file: str = "guesses.times.txt",
+    ):
+        self.guesses = Words().words.copy()
+        with open(guess_list_file) as f:
+            self.guesses += f.read().splitlines()
+        self.guesses = sorted(set(self.guesses))
+
+class GameState:
+    state: list[list[dict[str, str]]] = []
+
+    def __init__(
+        self,
+        hard_mode: bool = False,
+    ):
+        self.hard_mode = hard_mode
+
+    def update(self, guess: str, state: str):
+        update_state: list[dict[str, str]] = []
+        if len(guess) != 5:
+            raise Exception("Invalid guess length")
+        if len(state) != 5:
+            raise Exception("Invalid state length")
+        for n in range(5):
+            if state[n] == 'x':
+                update_state.append({'not': guess[n]})
+            elif state[n] == 'y':
+                update_state.append({'contains': guess[n]})
+            elif state[n] == 'g':
+                update_state.append({'is': guess[n]})
+            else:
+                raise Exception("Invalid state given")
+        self.state.append(update_state)
+
+    def next_best_guess(self) -> str:
+
+        #Filter out impossible guesses
+        words = word_filter(self)
+        #print(words)
+
+
+        #Run against remaining guesses
+        results = {}
+        for word in words:
+            next_state = GameState(hard_mode=self.hard_mode) 
+            next_state.state = self.state.copy()
+            game = Game(word=word,hard_mode=self.hard_mode)
+            for state in self.state:
+                guess = ""
+                for n in range(5):
+                    for key in state[n]:
+                        guess += state[n][key]
+                #print(guess)
+                game.guess(guess)
+            results[word] = len(game.remaining_words)
+            
+        #print(results)
+
+        # For now pick the one with the least remaining guesses
+        return [k for k, v in results.items() if v == min(results.values())][0]
+
+
+
+class Game:
     words: list[str] = []
     valid_guesses: list[str] = []
     word: str = ""
@@ -16,13 +94,9 @@ class Wordle:
         word: str = "",
         hard_mode: bool = False,
     ):
-        with open(word_list_file) as f:
-            self.words = f.read().splitlines()
+        self.words = Words(word_list_file=word_list_file).words
 
-        self.valid_guesses = self.words.copy()
-        with open(guess_list_file) as f:
-            self.valid_guesses += f.read().splitlines()
-        self.valid_guesses = sorted(set(self.valid_guesses))
+        self.valid_guesses = Guesses(guess_list_file=guess_list_file).guesses
 
         if word == "":
             self.word = random.choice(self.words)
@@ -110,6 +184,20 @@ class Wordle:
 
         self.state.append(guess_state)
 
+        guess_str = ""
+        for c in guess_state:
+            if 'not' in c.keys():
+                guess_str += "x"
+            elif 'contains' in c.keys():
+                guess_str += "y"
+            elif 'is' in c.keys():
+                guess_str += "g"
+            else:
+                raise Exception("Issue calculating state")
+
+        return guess_str
+
+
     def status(self) -> bool | None:
         if len(self.guesses) == 0:
             return None
@@ -119,3 +207,25 @@ class Wordle:
             return False
         else:
             return None
+
+def word_filter(game_state: GameState):
+    words = Words().words.copy()
+    for guess_state in game_state.state:
+        for n in range(5):
+            #print(n, guess_state[n])
+            if "is" in guess_state[n].keys():
+                words = [k for k in words if k[n] == guess_state[n]['is']]
+            elif "contains" in guess_state[n].keys():
+                words = [k for k in words if guess_state[n]['contains'] in k]
+                words = [k for k in words if k[n] != guess_state[n]['contains']]
+            elif "not" in guess_state[n].keys():
+                #Skip not if letter is previously contained
+                skip = False
+                for m in range(n):
+                    if "contains" in guess_state[m].keys() and guess_state[m]["contains"] == guess_state[n]['not']:
+                        skip = True
+                if not skip:    
+                    words = [k for k in words if guess_state[n]['not'] not in k]
+            #print(words)
+
+    return words
